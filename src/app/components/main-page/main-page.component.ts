5c0fcbc65c74bc1cfcf2ffa3e9ca08745e510537
Marina Kotova <m.klementyeva@mail.ru>
2025-03-11 15:10:14 +0700

    add layout and styles for main report page

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu'; 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
-
+    form: UntypedFormGroup = new UntypedFormGroup({});
+  
+    constructor(private fb: FormBuilder) {}
+  
+    ngOnInit(): void {
+      this.form = this.fb.group({
+        regionsArray: [{}, Validators.required],
+        region: [{}, Validators.required],
+      });
+    }
 }
