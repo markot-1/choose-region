2eec42dbd7745e2c12ec8da457f6af8cef477e24
Marina Kotova <m.klementyeva@mail.ru>
2025-03-12 10:41:34 +0700

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainPageComponent } from './components/main-page/main-page.component';
-
-export const routes: Routes = [
-    {path: 'main', component: MainPageComponent}
-];
--- src/app/validators/array-validators.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-export function minArrayLength(min: number) {
-  return (control: AbstractControl): ValidationErrors | null => {
-    return control.value && control.value.length >= min
-      ? null
-      : { minArrayLength: { requiredLength: min, actualLength: control.value.length } };
-  };
-}
