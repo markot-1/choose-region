e11a197fc3971340b535523fc53087c42861a468
Marina Kotova <m.klementyeva@mail.ru>
2025-03-13 19:33:15 +0700

    add redirect in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 
 export const routes: Routes = [
-    {path: 'main', component: MainPageComponent}
+    {path: 'main', component: MainPageComponent},
+    {path: '',   redirectTo: '/main', pathMatch: 'full'}
 ];
